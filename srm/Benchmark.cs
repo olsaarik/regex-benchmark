e3690c6c531245b3e578e121020d4ee5993b593a
Olli Saarikivi <olsaarik@microsoft.com>
2021-07-02 10:40:48 -0700

    Add SRM benchmark

---
+++ srm/Benchmark.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+using Microsoft.SRM;
+
+class Benchmark
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: benchmark <filename>");
+            Environment.Exit(1);
+        }
+
+        StreamReader reader = new System.IO.StreamReader(args[0]);
+        string data = reader.ReadToEnd();
+
+        // Email
+        Benchmark.Measure(data, @"[\w\.+-]+@[\w\.-]+\.[\w\.-]+");
+
+        // URI
+        Benchmark.Measure(data, @"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?");
+
+        // IP
+        Benchmark.Measure(data, @"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
+    }
+
+    static void Measure(string data, string pattern)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        var r = new Regex(pattern);
+        var matches = r.Matches(data);
+        int count = matches.Count;
+
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds.ToString("G", System.Globalization.CultureInfo.InvariantCulture) + " - " + count);
+    }
+}
